997db1fdfe7ec9d688202249bc8e3875de8a9d2f
John Nunemaker <nunemaker@gmail.com>
2013-01-07 16:45:36 -0500

    Added missing parenthesis.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ module MiniTest
     # Asserts the test case property is equal to the expected value. On failure
     # the message includes the property and user_agent_string from the test
     # case for easier debugging
-    def assert_test_case_property_equal test_case, actual, test_case_property
+    def assert_test_case_property_equal(test_case, actual, test_case_property)
       assert_equal test_case[test_case_property],
                    actual,
                    "#{test_case_property} failed for user agent: #{test_case['user_agent_string']}"
