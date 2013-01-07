333049108c243bc8cb571129d941cfc3f2011e8e
John Nunemaker <nunemaker@gmail.com>
2013-01-07 16:19:56 -0500

    Move parser specific setup methods to parser spec.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,19 +1,51 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe UserAgentParser::Parser do
+  def self.test_case_test_name(test_case)
+    user_agent = test_case['user_agent_string']
 
-  before do
-    @parser ||= UserAgentParser::Parser.new
-  end
-
-  def self.ua_string_to_test_name(ua)
     # Some Ruby versions (JRuby) need sanitised test names, as some chars screw
     # up the test method definitions
-    ua.gsub(/[^a-z0-9_.-]/i,'_')
+    user_agent.gsub(/[^a-z0-9_.-]/i, '_').squeeze('_')
   end
 
-  def self.test_case_test_name(tc)
-    ua_string_to_test_name(tc['user_agent_string'])
+  def self.ua_parser_test_cases
+    parser_test_cases("test_user_agent_parser") +
+    parser_test_cases("firefox_user_agent_strings") +
+    parser_test_cases("pgts_browser_list")
+  end
+
+  def self.os_parser_test_cases
+    parser_test_cases("test_user_agent_parser_os") +
+    parser_test_cases("additional_os_tests")
+  end
+
+  def self.parser_test_cases(file)
+    yaml_test_resource(file)['test_cases'].map do |test_case|
+      {
+        'user_agent_string' => test_case['user_agent_string'],
+        'family'            => test_case['family'],
+        'major'             => test_case['major'],
+        'minor'             => test_case['minor'],
+        'patch'             => test_case['patch'],
+        'patch_minor'       => test_case['patch_minor'],
+      }
+    end.reject do |test_case|
+      # We don't do the hacky javascript user agent overrides
+      test_case.key?('js_ua') ||
+        test_case['family'] == 'IE Platform Preview' ||
+        test_case['user_agent_string'].include?('chromeframe;')
+    end
+  end
+
+  def self.yaml_test_resource(resource)
+    test_resource_path = File.expand_path('../../vendor/ua-parser/test_resources', __FILE__)
+    resource_path = File.join(test_resource_path, "#{resource}.yaml")
+    YAML.load_file(resource_path)
+  end
+
+  def parser
+    @parser ||= UserAgentParser::Parser.new
   end
 
   describe "#initialize" do
@@ -31,7 +63,7 @@ describe UserAgentParser::Parser do
   describe "#parse" do
     ua_parser_test_cases.each do |tc|
       it "parses UA for #{test_case_test_name(tc)}" do
-        ua = @parser.parse(tc['user_agent_string'])
+        ua = UserAgentParser::Parser.new.parse(tc['user_agent_string'])
 
         if tc['family']
           ua.family.must_equal_test_case_property(tc, 'family')
@@ -53,7 +85,7 @@ describe UserAgentParser::Parser do
 
     os_parser_test_cases.each do |tc|
       it "parses OS for #{test_case_test_name(tc)}" do
-        ua = @parser.parse(tc['user_agent_string'])
+        ua = parser.parse(tc['user_agent_string'])
 
         if tc['family']
           ua.os.name.must_equal_test_case_property(tc, 'family')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,47 +5,6 @@ require 'json'
 $:.unshift File.join(File.dirname(__FILE__), "../lib")
 require 'user_agent_parser'
 
-def ua_parser_test_cases
-  parser_test_cases("test_user_agent_parser") +
-  parser_test_cases("firefox_user_agent_strings") +
-  parser_test_cases("pgts_browser_list")
-end
-
-def os_parser_test_cases
-  parser_test_cases("test_user_agent_parser_os") +
-  parser_test_cases("additional_os_tests")
-end
-
-def parser_test_cases(file)
-  yaml_test_resource(file)['test_cases'].map do |tc|
-    {
-      'user_agent_string' => tc['user_agent_string'],
-      'family'      => tc['family'],
-      'major'       => tc['major'],
-      'minor'       => tc['minor'],
-      'patch'       => tc['patch'],
-      'patch_minor' => tc['patch_minor'],
-    }
-  end.reject do |tc|
-    # We don't do the hacky javascript user agent overrides
-    tc['js_ua']
-  end.reject do |tc|
-    # For the above reason, we don't detect the IE Platform Preview cases
-    tc['family'] == 'IE Platform Preview'
-  end.reject do |tc|
-    # Same goes for the older chromeframe UA's (the ones without versions)
-    tc['user_agent_string'].include?('chromeframe;')
-  end
-end
-
-def yaml_test_resource(resource)
-  YAML.load_file test_resource_path(resource + ".yaml")
-end
-
-def test_resource_path resource
-  File.join File.dirname(__FILE__), "../vendor/ua-parser/test_resources", resource
-end
-
 module MiniTest
   module Assertions
     # Asserts the test case property is equal to the expected value. On failure
