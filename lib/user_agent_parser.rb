91e8a522472183698e925cfa6a9eff729423926a
John Nunemaker <nunemaker@gmail.com>
2013-01-10 10:40:22 -0500

    Remove .parse class method.

--- lib/user_agent_parser.rb
+++ lib/user_agent_parser.rb
@@ -16,9 +16,4 @@ module UserAgentParser
   end
 
   self.patterns_path = File.join(File.dirname(__FILE__), "../vendor/ua-parser/regexes.yaml")
-
-  # Parse the given +user_agent_string+, returning a +UserAgent+
-  def self.parse(user_agent_string)
-    Parser.new.parse(user_agent_string)
-  end
 end
